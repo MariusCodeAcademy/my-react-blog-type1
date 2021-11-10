6a3a0b261067bf2021f9d3d0655b097d284c8f75
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-10 10:49:46 +0200

    normalContext

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,39 @@
+import { useState, useContext } from 'react';
+import AuthContext from '../../store/AuthContext';
+
 function Login() {
-  return <div>Login</div>;
+  const [userName, setUserName] = useState('');
+  const authCtx = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!userName) {
+      return console.error('add username');
+    }
+    console.log('login action in handle submit');
+    authCtx.login(userName);
+  };
+
+  const styles = {
+    border: '1px solid #333',
+    padding: '1rem',
+    display: 'inline-block',
+  };
+
+  return (
+    <div style={styles}>
+      <h4>Login here</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+          type='text'
+          placeholder='username'
+        />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
